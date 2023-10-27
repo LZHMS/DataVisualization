5d6aeb7016d4e4823810beb2a34442a58c365c67
LZHMS <mark13289817328@gmail.com>
2023-10-27 18:44:32 +0800

    Update FlowMap

--- website/src/views/Home/Home.js
+++ website/src/views/Home/Home.js
@@ -4,6 +4,7 @@ import SpiralCurve from '../SpiralCurve/SpiralCurve';
 import NormalSurface from '../NormalSurface/NormalSurface';
 import Sphere from '../Sphere/Sphere';
 import MapGraph from '../MapGraph/MapGraph';
+import FlowMap from '../FlowMap/FlowMap';
 import "./index.module.css"
 
 
@@ -11,6 +12,7 @@ class Home extends Component {
     render () {
         return (
             <div>
+                <div className='App'><FlowMap /></div>
                 <div className='App'><MapGraph /></div>
                 <div className='App'><Sphere /></div>
                 <div className='App'><NormalSurface /></div>
